0eef3cab637cfeb5dc315110efbe1308b10e400a
Sepiolx64 <62660052+Sepiolx64@users.noreply.github.com>
2023-04-19 17:31:27 +0200

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo -e "\nPlease provide an element as an argument.\n"
+
+ELEMENTS=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number")
+echo "$ELEMENTS" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS
+do
+  echo 1
+  #echo "The element with $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+done
+
+if [[ $1 || $H ]]
+  then
+    echo $1
+fi
