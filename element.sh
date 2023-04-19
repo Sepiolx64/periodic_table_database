fa890c05e85a3e2e17fa913aa5869b0132b1a6fc
Sepiolx64 <62660052+Sepiolx64@users.noreply.github.com>
2023-04-19 22:26:28 +0200

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo -e "\nPlease provide an element as an argument.\n"
+SELECT_ELEMENT() {
+  ELEMENTS=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number WHERE elements.atomic_number=$1")
+  
+  echo "$ELEMENTS" | while IFS='|' read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+  done
+}
 
-ELEMENTS=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties ON elements.atomic_number=properties.atomic_number")
-echo "$ELEMENTS" | while read ATOMIC_NUMBER BAR NAME BAR SYMBOL BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS
-do
-  echo 1
-  #echo "The element with $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-done
-
-if [[ $1 || $H ]]
-  then
-    echo $1
+if [[ $1 == 1 || $1 == 'H' || $1 == 'Hydrogen' ]]
+then
+  SELECT_ELEMENT 1
+else
+  SELECT_ELEMENT $1
 fi
